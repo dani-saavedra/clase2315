3c1684f596e9510dc503e9d535fa94e7fecd6bde
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-09-08 17:48:42 -0500

    Fix conflict in class Calculo

--- src/main/java/co/edu/unisabana/siga/logica/Calculo.java
+++ src/main/java/co/edu/unisabana/siga/logica/Calculo.java
@@ -5,9 +5,6 @@ public class Calculo {
   public void sumar(int num1, int num2) {
     System.out.println(num1 + num2);
   }
-  public void restar(int num1, int num2) {
-    System.out.println(num1 - num2);
-  }
   public void multiplicacion(int num1, int num2) {
     System.out.println(num1 * num2);
   }
