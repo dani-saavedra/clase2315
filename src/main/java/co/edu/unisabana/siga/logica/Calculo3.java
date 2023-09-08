df854441af8f2109faaba0f62110a909dd375e93
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-09-08 17:54:03 -0500

    Calculo 3

---
+++ src/main/java/co/edu/unisabana/siga/logica/Calculo3.java
@@ -0,0 +1,11 @@
+package co.edu.unisabana.siga.logica;
+
+public class Calculo3 {
+
+  public void sumar(int num1, int num2) {
+    System.out.println(num1 + num2);
+  }
+  public void multiplicacion(int num1, int num2) {
+    System.out.println(num1 * num2);
+  }
+}
