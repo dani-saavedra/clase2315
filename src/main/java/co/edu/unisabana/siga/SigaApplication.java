58123a747b537c7461e74e6794a8b7ee5bb111dd
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-09-20 18:08:28 -0500

    bla bla bla

--- src/main/java/co/edu/unisabana/siga/SigaApplication.java
+++ src/main/java/co/edu/unisabana/siga/SigaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SigaApplication {
+public class SigaApplication
 
 	public static void main(String[] args) {
 		SpringApplication.run(SigaApplication.class, args);
