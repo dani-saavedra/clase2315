8000a57ddc572d9c4daec789bdde5030d60c8fb9
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2023-09-20 18:14:55 -0500

    Fix X

--- src/main/java/co/edu/unisabana/siga/SigaApplication.java
+++ src/main/java/co/edu/unisabana/siga/SigaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SigaApplication
+public class SigaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SigaApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SigaApplication.class, args);
+  }
 
 }
